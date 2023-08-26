f1062e5ac5f5c4453ec01a31291e28d2f3db46dd
Obinna <macmartins081@gmail.com>
2023-08-27 00:59:39 +0400

    Created high level components with layout.

---
+++ tip-calculator/View/BillInputView.swift
@@ -0,0 +1,24 @@
+//
+//  BillInputView.swift
+//  tip-calculator
+//
+//  Created by Obinna on 27/08/2023.
+//
+
+import UIKit
+
+class BillInputView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .purple
+    }
+}
---
+++ tip-calculator/View/LogoView.swift
@@ -0,0 +1,24 @@
+//
+//  LogoView.swift
+//  tip-calculator
+//
+//  Created by Obinna on 27/08/2023.
+//
+
+import UIKit
+
+class LogoView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .red
+    }
+} 
