8dd561d4fc356d145a7271cd5819769b8796b115
Obinna <macmartins081@gmail.com>
2023-09-01 22:17:37 +0400

    Added headerView, textFieldContainerView, textField, and cornerRadius helper.

--- tip-calculator/View/BillInputView.swift
+++ tip-calculator/View/BillInputView.swift
@@ -9,6 +9,45 @@ import UIKit
 
 class BillInputView: UIView {
     
+    private let headerView: HeaderView = {
+        return HeaderView()
+    }()
+    
+    private let textFieldContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.addCornerRadius(radius: 8.0)
+        return view
+    }()
+    
+    private let currencyDenominationLabel: UILabel = {
+        let label = LabelFactory.build(
+            text: "$",
+            font: ThemeFont.bold(ofSize: 24))
+        label.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        return label
+    }()
+    
+    private lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.font = ThemeFont.demibold(ofSize: 28)
+        textField.keyboardType = .decimalPad
+        textField.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        textField.tintColor = ThemeColor.text
+        textField.textColor = ThemeColor.text
+        // Add toolbar
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: 36))
+        toolbar.barStyle = .default
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(
+            title: "Done",
+            style: .plain,
+            target: self,
+            action: #selector(doneButtonTapped))
+        return textField
+    }()
+    
     init() {
         super.init(frame: .zero)
         layout()
@@ -20,5 +59,25 @@ class BillInputView: UIView {
     
     private func layout() {
         backgroundColor = .purple
+    }
+    
+    @objc private func doneButtonTapped() {
+        
+    }
+}
+
+class HeaderView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .red
     }
 }
